c345b744d862fd65d19fe00f97ecab00934df417
Yilun Chen <yilunc@berkeley.edu>
2015-08-09 16:12:11 -0700

    added arrow movement

--- javascript/app.js
+++ javascript/app.js
@@ -2,12 +2,19 @@
 
 
   var activeImage;
+  var isDragging = false
   var startingMouseLocation = {x: 0, y: 0};
   var originalImageLocation = {x: 0, y: 0};
+
+  var applydelta = function (delta) {
+    activeImage.css('top', originalImageLocation.y + delta.y)
+    activeImage.css('left', originalImageLocation.x + delta.x)
+  }
 
   $('img').mousedown(function (event) {
     event.preventDefault();
     activeImage = $(event.currentTarget);
+    isDragging = true;
     startingMouseLocation = {x: event.clientX, y: event.clientY};
     originalImageLocation = {
       x: parseInt(activeImage.css('left')),
@@ -17,20 +24,39 @@
 
   $('img').mousemove(function (event) {
     event.preventDefault();
-    if (activeImage) {
+    if (isDragging) {
       var mouseLocation = {x: event.clientX, y: event.clientY};
       var mouseDelta = {
         x: mouseLocation.x - startingMouseLocation.x,
         y: mouseLocation.y - startingMouseLocation.y
       };
-
-      activeImage.css('top', originalImageLocation.y + mouseDelta.y)
-      activeImage.css('left', originalImageLocation.x + mouseDelta.x)
+      applydelta(mouseDelta);
     }
-  })
+  });
 
   $('img').mouseup(function () {
     event.preventDefault();
-    activeImage = null;
+    isDragging = false;
+  });
+
+  $(window).keydown(function (event){
+    var key = event.keyCode;
+    var arrowDelta;
+
+    switch(key) {
+      case 37: // left
+        arrowDelta = {x: -1, y: 0}
+        break;
+      case 38: // up
+        arrowDelta = {x: 0, y: 1}
+        break;
+      case 39: // right
+        arrowDelta = {x: 1, y: 0}
+        break;
+      case 40: // down
+        arrowDelta = {x: 0, y: -1}
+        break;
+    }
+    applydelta(arrowDelta)
   });
 
